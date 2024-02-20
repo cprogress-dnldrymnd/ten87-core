7caf8255a7276d10b40021732242b79b44f68876
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-21 05:46:03 +0800

    css

--- inc/post-types/team/shortcodes/team-list/templates/post-info/excerpt.php
+++ inc/post-types/team/shortcodes/team-list/templates/post-info/excerpt.php
@@ -6,7 +6,7 @@ if ( post_password_required() ) {
 	$excerpt = obsius_core_get_custom_post_type_excerpt( isset( $excerpt_length ) ? $excerpt_length : '' );
 
 	if ( ! empty( $excerpt ) ) { ?>
-		<p itemprop="description" class="qodef-e-excerpt"><?php echo esc_html( $excerpt ); ?></p>
+		<p itemprop="description" class="qodef-e-excerpt"><?php echo esc_html( get_the_content() ); ?></p>
 		<?php
 	}
 }
