9152f14bdc52c4a8183fc5eaf43ea3c3b44b41bc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-10 17:46:23 +0800

    Update excerpt.php

--- inc/post-types/team/shortcodes/team-list/templates/post-info/excerpt.php
+++ inc/post-types/team/shortcodes/team-list/templates/post-info/excerpt.php
@@ -1,16 +1,18 @@
 <?php
 
-if ( post_password_required() ) {
+if (post_password_required()) {
 	echo get_the_password_form(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 } else {
-	$excerpt = obsius_core_get_custom_post_type_excerpt( isset( $excerpt_length ) ? $excerpt_length : '' );
+	$excerpt = obsius_core_get_custom_post_type_excerpt(isset($excerpt_length) ? $excerpt_length : '');
 
-	if ( ! empty( $excerpt ) ) { ?>
-		<p itemprop="description" class="qodef-e-excerpt"><?php echo esc_html( get_the_content() ); ?></p>
-		<p>
-			<button class="modal-trigger" >Read More</a> </button>
-		</p>
-		<?php
+	if (!empty($excerpt)) { ?>
+		<p itemprop="description" class="qodef-e-excerpt"><?php echo esc_html(get_the_content()); ?></p>
+		<?php if (is_archive()) { ?>
+			<p>
+				<button class="modal-trigger">Read More</a> </button>
+			</p>
+		<?php } ?>
+<?php
 	}
 }
 ?>
